f79043c478fa71b979645c539959f7d60f5e3c15
Rashi Singh <rashi170101052@iitg.ac.in>
2020-05-24 22:55:05 +0530

    Added time input option

--- youtube_start_video_after_15_min.sh
+++ youtube_start_video_after_15_min.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 read -p "Youtube address? " address
+read -p "Restart after how many (+ve int) minutes? " min
 page=$(wget "$address" -O - 2>/dev/null)
 title=$(echo "$page" | grep "<title>")
 title=$(echo "$title" | tr "\n" "|" | grep -o '<title>.*</title>' | sed 's/\(<title>\|<\/title>\)//g;s/|/\n/g')
 # title=$(echo "$title" | grep -o '<title>.*</title>' | sed 's/\(<title>\|<\/title>\)//g')
+echo "Starting video"
 while [ 1 ]; do
-	echo "Starting video"
 	google-chrome --new-window "$address"
-	sleep 1200
+	sleep `expr $min \* 60`
 	echo "reopening window now"
-	wmctrl -c "$title"
+	wmctrl -c $title
 done
